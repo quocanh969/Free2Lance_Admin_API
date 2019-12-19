ad26f3d3c5b71b28732406f9fa091c0de2f09690
unknown <vincentlam30111998@gmail.com>
2019-12-19 23:45:57 +0700

    Query updated for major model

--- models/majorModel.js
+++ models/majorModel.js
@@ -9,12 +9,12 @@ module.exports = {
         return db.query(`select * from majors where id = ${id}`);
     },
     updateMajor: (id, majorObj) => {
-        return db.query(`update majors set name = '${majorObj.name}', icoUrl = '${majorObj.icoUrl}' where id = ${majorObj.id_major}`);
+        return db.query(`update majors set name = '${majorObj.name}', icoUrl = '${majorObj.icoUrl}', isEditting = ${false} where id = ${majorObj.id_major}`);
     },
     deleteMajor: (id, id_major) => {
-        return db.query(`update majors set status = ${false} where id = ${id_major}`);
+        return db.query(`update majors set status = ${false}, isEditting = ${false} where id = ${id_major}`);
     },
     recoverMajor: (id, id_major) => {
-        return db.query(`update majors set status = ${true} where id = ${id_major}`);
+        return db.query(`update majors set status = ${true}, isEditting = ${false} where id = ${id_major}`);
     }
 }
